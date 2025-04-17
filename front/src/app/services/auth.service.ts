6f5003fce4a06b066e71f30cfc85ae0dfa751ba8
flo <flo54@gmx.fr>
2025-04-17 15:29:25 +0200

    (fix) Token in localStorage fix

--- front/src/app/services/auth.service.ts
+++ front/src/app/services/auth.service.ts
@@ -2,6 +2,7 @@ import {BehaviorSubject, Observable, tap} from "rxjs";
 import {HttpClient} from "@angular/common/http";
 import {LoginRequestInterface} from "../interfaces/login/login-request.interface";
 import {Injectable} from "@angular/core";
+import {LoginResponseInterface} from "../interfaces/login/login-response.interface";
 
 @Injectable( {
   providedIn: 'root'
@@ -16,13 +17,14 @@ export class AuthService {
   }
   constructor(private httpClient: HttpClient) { }
 
-  public login(loginRequestInterface: LoginRequestInterface): Observable<String> {
+  public login(loginRequestInterface: LoginRequestInterface): Observable<LoginResponseInterface> {
 
-    return this.httpClient.post<string>('api/auth/login', loginRequestInterface).pipe(
-      tap((response: string) => {
+    return this.httpClient.post<LoginResponseInterface>('api/auth/login', loginRequestInterface).pipe(
+      tap((response: LoginResponseInterface) => {
         this.isLogged = true;
         this.isLoggedSubject.next(this.isLogged);
-        localStorage.setItem('token', response);
+        if(response.token) localStorage.setItem('token', response.token);
+
       })
     );
   }
