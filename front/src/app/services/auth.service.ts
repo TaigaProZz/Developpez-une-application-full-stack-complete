4ddf438b9c3fdd4a77161827422cbb851522de0e
flo <flo54@gmx.fr>
2025-04-17 17:44:51 +0200

    (feat) store token on successfully register

--- front/src/app/services/auth.service.ts
+++ front/src/app/services/auth.service.ts
@@ -3,6 +3,8 @@ import {HttpClient} from "@angular/common/http";
 import {LoginRequestInterface} from "../interfaces/login/login-request.interface";
 import {RegisterRequestInterface} from "../interfaces/register/register-request.interface";
 import {Injectable} from "@angular/core";
+import {LoginResponseInterface} from "../interfaces/login/login-response.interface";
+import {RegisterResponseInterface} from "../interfaces/register/register-response.interface";
 
 @Injectable( {
   providedIn: 'root'
@@ -17,18 +19,28 @@ export class AuthService {
   }
   constructor(private httpClient: HttpClient) { }
 
-  public login(loginRequestInterface: LoginRequestInterface): Observable<String> {
+  public login(loginRequestInterface: LoginRequestInterface): Observable<LoginResponseInterface> {
 
-    return this.httpClient.post<string>('api/auth/login', loginRequestInterface).pipe(
-      tap((response: string) => {
+    return this.httpClient.post<LoginResponseInterface>('api/auth/login', loginRequestInterface).pipe(
+      tap((response: LoginResponseInterface) => {
         this.isLogged = true;
         this.isLoggedSubject.next(this.isLogged);
-        localStorage.setItem('token', response);
+        if(response && response.token) {
+          localStorage.setItem('token', response.token);
+        }
       })
     );
   }
 
-  public register(registerRequestInterface: RegisterRequestInterface): Observable<void> {
-    return this.httpClient.post<void>('api/auth/register', registerRequestInterface);
+  public register(registerRequestInterface: RegisterRequestInterface): Observable<RegisterResponseInterface> {
+    return this.httpClient.post<RegisterResponseInterface>('api/auth/register', registerRequestInterface).pipe(
+      tap((response: RegisterResponseInterface) => {
+        this.isLogged = true;
+        this.isLoggedSubject.next(this.isLogged);
+        if(response && response.token) {
+          localStorage.setItem('token', response.token);
+        }
+      })
+    );
   }
 }
