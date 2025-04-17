94d601d6c57ef0477361123a3bc7643d3d98273f
flo <flo54@gmx.fr>
2025-04-17 19:40:17 +0200

    (feat angular) auth guard to protect routes who needs login

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AuthService} from "./services/auth.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'front';
+  isLogged: boolean = false;
+
+  constructor(private authService: AuthService) {}
+
+  ngOnInit() {
+    this.authService.$isLogged().subscribe((isLoggedIn: boolean) => {
+      this.isLogged = isLoggedIn;
+    });
+  }
 
   public displayNavbar(): boolean {
     const currentUrl = window.location.pathname;
