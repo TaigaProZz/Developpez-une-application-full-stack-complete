1a1e0b82e998ffc6cf7a59860b05d15522281d03
flo <flo54@gmx.fr>
2025-04-18 15:31:13 +0200

    (feat angular) auth guard and guest guard updated to autolog

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -3,13 +3,17 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import {LoginComponent} from "./pages/login/login.component";
 import {RegisterComponent} from "./pages/register/register.component";
+//import {ProfileComponent} from "./pages/profile/profile.component";
+import {AuthGuard} from "./guards/auth-guard";
+import {GuestGuard} from "./guards/guest-guard";
 
 // consider a guard combined with canLoad / canActivate route option
 // to manage unauthenticated user to access private routes
 const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent},
+  { path: '', component: HomeComponent, canActivate: [GuestGuard] },
+  { path: 'login', component: LoginComponent, canActivate: [GuestGuard] },
+  { path: 'register', component: RegisterComponent, canActivate: [GuestGuard]},
+  //{ path: 'profile', component: ProfileComponent, canActivate: [AuthGuard] },
 ];
 
 @NgModule({
